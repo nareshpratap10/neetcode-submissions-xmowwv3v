b7059b3cfe5d2aec245eac592d6aa6b9d641689a
nareshpratap10 <59041348+nareshpratap10@users.noreply.github.com>
2026-04-07 00:24:10 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!= t.length()){
+            return false;
+        }
+        vector<int> vs(26,0);
+        vector<int> vt(26,0);
+
+        for(const char& x : s){
+            vs[x-'a']++;
+        }
+        for(const char& x : t){
+            vt[x-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(vt[i]!=vs[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+};
