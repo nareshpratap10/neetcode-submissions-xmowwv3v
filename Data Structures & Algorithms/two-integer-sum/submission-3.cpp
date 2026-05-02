e0f4003ad8a0e7da9c39c213725f9eb9bc9cc6d0
nareshpratap10 <59041348+nareshpratap10@users.noreply.github.com>
2026-05-02 11:19:36 +0530

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int,int> umap(n);
+        
+
+        for(int i=0;i<n;i++){
+            if(umap.find(target-nums[i])!=umap.end()){
+                return {umap[target-nums[i]],i};
+            }
+            umap[nums[i]]=i;
+        }
+        return {};
+
+    }
+};
